83e20ef0e2052da48a56a5f57050228cffc30757
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-10 14:54:42 +0530

    Update show_videos_page.dart

--- lib/screens/show_videos_page.dart
+++ lib/screens/show_videos_page.dart
@@ -15,7 +15,7 @@ class _ShowVideosState extends State<ShowVideos> {
   Map<String, List<String>> emotionToVideo = {
     'Happy': [
       'https://www.youtube.com/watch?v=CvaPziI1SPc',
-      'https://www.youtube.com/watch?v=5qap5aO4i9A',
+      'https://www.youtube.com/watch?v=JxJsai5nkGI',
       'https://www.youtube.com/watch?v=5qap5aO4i9A',
       'https://www.youtube.com/watch?v=5qap5aO4i9A',
       'https://www.youtube.com/watch?v=5qap5aO4i9A',
