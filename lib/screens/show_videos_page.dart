64a1b9cee0a11ad8b79deaf36869f8dbfc87cb43
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-10 14:44:11 +0530

    Update show_videos_page.dart

--- lib/screens/show_videos_page.dart
+++ lib/screens/show_videos_page.dart
@@ -27,6 +27,13 @@ class _ShowVideosState extends State<ShowVideos> {
       'https://www.youtube.com/watch?v=i1jSCpo1Vq0',
       'https://www.youtube.com/watch?v=i1jSCpo1Vq0',
     ],
+    'Angry': [
+      'https://www.youtube.com/watch?v=5qap5aO4i9A',
+      'https://www.youtube.com/watch?v=5qap5aO4i9A',
+      'https://www.youtube.com/watch?v=5qap5aO4i9A',
+      'https://www.youtube.com/watch?v=5qap5aO4i9A',
+      'https://www.youtube.com/watch?v=5qap5aO4i9A',
+    ],
   };
 
   @override
